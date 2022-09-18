ea2b8e45d2fa5bcb3d0d44d8254e447ea9df5afc
Arlindo Garcia <arlindogarciagomesneto70@gmail.com>
2022-09-18 15:47:42 -0300

    Feat: classe de erros e middleware de gerenciamento de erros

--- src/shared/http/app.ts
+++ src/shared/http/app.ts
@@ -1,18 +1,44 @@
-import express from 'express';
+import express, { Express, NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import routes from './routes';
+import AppError from '@shared/errors/AppError';
 
 class App {
-  server: any;
+  public server: Express;
+
   constructor() {
     this.server = express();
     this.middlewares();
     this.routes();
+    this.middlewaresErrors();
   }
 
   middlewares() {
     this.server.use(cors());
     this.server.use(express.json());
+  }
+
+  middlewaresErrors() {
+    this.server.use(
+      (
+        error: Error,
+        request: Request,
+        response: Response,
+        next: NextFunction,
+      ) => {
+        if (error instanceof AppError) {
+          return response.status(error.statusCode).json({
+            status: 'error',
+            message: error.message,
+          });
+        }
+
+        return response.status(500).json({
+          status: 'error',
+          message: 'Internal server error',
+        });
+      },
+    );
   }
 
   routes() {
