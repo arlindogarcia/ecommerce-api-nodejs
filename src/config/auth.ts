82f3fd8241d63845d2875211208b2d2b85b4a911
Arlindo Garcia <arlindogarciagomesneto70@gmail.com>
2022-09-19 00:47:07 -0300

    Feat: middleware para verificação de authenticação de usuário

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: 'ac766dd546859819da978f755382e95b',
+    expiresIn: '1d',
+  },
+};
--- src/modules/users/services/CreateSessionsService.ts
+++ src/modules/users/services/CreateSessionsService.ts
@@ -4,6 +4,7 @@ import { getCustomRepository } from 'typeorm';
 import User from '../typeorm/entities/User';
 import UsersRepository from '../typeorm/repositores/UsersRepository';
 import { sign } from 'jsonwebtoken';
+import authConfig from '@config/auth';
 
 interface IRequest {
   email: string;
@@ -31,9 +32,9 @@ class CreateSessionsService {
       throw new AppError('Incorrect email/password combination', 401);
     }
 
-    const token = sign({}, 'ac766dd546859819da978f755382e95b', {
+    const token = sign({}, authConfig.jwt.secret, {
       subject: user.id,
-      expiresIn: '1d',
+      expiresIn: authConfig.jwt.expiresIn,
     });
 
     return { user, token };
