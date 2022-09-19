ac08deb2560a70032a55a0a3a8e26d1412bcd124
Arlindo Garcia <arlindogarciagomesneto70@gmail.com>
2022-09-19 00:27:20 -0300

    Feat: controller de sessão, implementação do JWT

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,10 +1,12 @@
 import productsRouter from '@modules/products/routes/products.routes';
-import usersRoutes from '@modules/users/router';
+import sessionsRouter from '@modules/users/router/sessions.routes';
+import usersRoutes from '@modules/users/router/users.routes';
 import { Router } from 'express';
 
 const routes = Router();
 
 routes.use('/products', productsRouter);
 routes.use('/users', usersRoutes);
+routes.use('/sessions', sessionsRouter);
 
 export default routes;
