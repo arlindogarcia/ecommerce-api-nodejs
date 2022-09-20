e2ec3bd19e04888cc938c1d5ab546aaceca706fd
Arlindo Garcia <arlindogarciagomesneto70@gmail.com>
2022-09-19 21:19:31 -0300

    Feat: Função de criação de tokens para ForgotPaasswords

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,4 +1,5 @@
 import productsRouter from '@modules/products/routes/products.routes';
+import passwordRouter from '@modules/users/router/password.routes';
 import sessionsRouter from '@modules/users/router/sessions.routes';
 import usersRoutes from '@modules/users/router/users.routes';
 import { Router } from 'express';
@@ -8,5 +9,6 @@ const routes = Router();
 routes.use('/products', productsRouter);
 routes.use('/users', usersRoutes);
 routes.use('/sessions', sessionsRouter);
+routes.use('/password', passwordRouter);
 
 export default routes;
