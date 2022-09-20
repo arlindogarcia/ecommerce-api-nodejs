3bacf39e4ff429ab398429d3910b9da3bc24a4b2
Arlindo Garcia <arlindogarciagomesneto70@gmail.com>
2022-09-20 00:26:20 -0300

    Feat: Perfil do usuário, show e update, controller, serviços e rotas

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,5 +1,6 @@
 import productsRouter from '@modules/products/routes/products.routes';
 import passwordRouter from '@modules/users/router/password.routes';
+import profileRouter from '@modules/users/router/profile.routes';
 import sessionsRouter from '@modules/users/router/sessions.routes';
 import usersRoutes from '@modules/users/router/users.routes';
 import { Router } from 'express';
@@ -10,5 +11,6 @@ routes.use('/products', productsRouter);
 routes.use('/users', usersRoutes);
 routes.use('/sessions', sessionsRouter);
 routes.use('/password', passwordRouter);
+routes.use('/profile', profileRouter);
 
 export default routes;
