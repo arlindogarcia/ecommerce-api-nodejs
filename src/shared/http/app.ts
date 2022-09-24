df910fa6ad7999c67154b0fef88b940fbfc0a2a7
Arlindo Garcia <arlindogarciagomesneto70@gmail.com>
2022-09-24 00:09:10 -0300

    Feat: add typeorm-pagination

--- src/shared/http/app.ts
+++ src/shared/http/app.ts
@@ -3,6 +3,7 @@ import express, { Express, NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
 import cors from 'cors';
 import routes from './routes';
+import { pagination } from 'typeorm-pagination';
 import AppError from '@shared/errors/AppError';
 import '@shared/typeorm';
 import { errors } from 'celebrate';
@@ -14,6 +15,7 @@ class App {
   constructor() {
     this.server = express();
     this.config();
+    this.middlewarePagination();
     this.routes();
     this.middlewaresErrors();
   }
@@ -51,6 +53,10 @@ class App {
   routes() {
     this.server.use('/files', express.static(uploadConfig.directory));
     this.server.use(routes);
+  }
+
+  middlewarePagination() {
+    this.server.use(pagination);
   }
 }
 
